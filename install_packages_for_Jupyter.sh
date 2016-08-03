fc9e55469516723dd0d03fba3eb142288d73524c
Artem Kupriyanov <kupriyanovartem@yandex.ru>
2016-08-03 21:43:28 +0300

    update main script’s

--- install_packages_for_Jupyter.sh
+++ install_packages_for_Jupyter.sh
@@ -8,13 +8,16 @@ exit
 sudo /etc/init.d/ssh restart
 # install packages
 sudo apt-get update
-sudo apt-get -qq install -y make build-essential git
+sudo apt-get -qq install -y make build-essential git zip python3-pip
 wget -c http://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
 bash Anaconda3-4.1.1-Linux-x86_64.sh -b -p ~/anaconda/
 export PATH=~/anaconda/bin:$PATH
-conda update -y --all
-conda install -y gensim
-pip install -y xgboost
+conda update --all --yes
+conda install --yes gensim
+conda install --yes seaborn
+export PATH=~/anaconda/bin:$PATH
+sudo apt-get -y install python-pip
+pip install xgboost
 echo -e "from IPython.lib import passwd\nimport sys\nsys.stdout.write(passwd('$passwd'))" > passwd.py
 python passwd.py > sha.txt
 openssl req -batch -x509 -nodes -days 365 -newkey rsa:1024 -keyout mykey.key -out mycert.pem
@@ -25,4 +28,7 @@ echo -e -n "# Set options for certficate, ip, password, and toggle off browser a
 echo -e -n "'\nc.NotebookApp.open_browser = False\n# It is a good idea to set a known, fixed port for server access\nc.NotebookApp.port = 8888\n" > second_parcel.txt
 cat first_parcel.txt sha.txt second_parcel.txt > jupyter_notebook_config.py
 cd ~
+export PATH=~/anaconda/bin:$PATH
+wget https://codeload.github.com/ArtemKupriyanov/MACHINE-LEARNING/zip/master
+unzip master
 jupyter notebook
--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,7 @@
 source 'config.txt'
 # загружаем некоторые скрипты на сервер.
 # Могут понадобиться для дальнейшей работы
-sh load_file_with_pem.sh ./save_file.sh ./install_packages_for_Jupyter.sh ./config.txt
 scp -i $pemfile ./install_packages_for_Jupyter.sh ubuntu@$publicIP:~/
-scp -i $pemfile ./config.txt.sh ubuntu@$publicIP:~/
+scp -i $pemfile ./config.txt ubuntu@$publicIP:~/
 # Подключаемся к серверу и в нем устанавливаем нужные пакеты для jupyter
 ssh -i $pemfile ubuntu@$publicIP 'bash -s' < install_packages_for_Jupyter.sh
